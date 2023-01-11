0d1408f777d268ce504869b064dc34785cc8d408
mingyo0125 <wjdalsry125@gmail.com>
2023-01-11 12:40:34 +0900

    ClingClimb Fin

--- SpiderMan/Assets/1.Scripts/PlayerController.cs
+++ SpiderMan/Assets/1.Scripts/PlayerController.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rigidbody;
+    Rigidbody  rigidbody;
     Camera cam;
+    RaycastHit hit;
+    public LayerMask layerMask;
 
     [SerializeField] float moveSpeed = 10;
     [SerializeField] float rotateSpeed = 3f;
     [SerializeField] float currentRotate = 0;
+
+    [SerializeField] GameObject player;
+    
+    bool canCling;
+    public bool canClimbing;
+
+    float inputX;
+    float inputZ;
+    float mouseX;
+    float mouseY;
 
     private void Awake()
     {
@@ -19,14 +31,23 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        Cling();
         PlayerMove();
         RotateCamera();
+
+        if (canClimbing)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+            }
+        }
     }
 
-    private void PlayerMove()
+    public void PlayerMove()
     {
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputZ = Input.GetAxisRaw("Vertical");
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputZ = Input.GetAxisRaw("Vertical");
 
         Vector3 moveHorizontal = transform.right * inputX;
         Vector3 moveVertical = transform.forward * inputZ;
@@ -34,12 +55,13 @@ public class PlayerController : MonoBehaviour
         Vector3 velo = (moveHorizontal + moveVertical).normalized * moveSpeed;
 
         rigidbody.MovePosition(transform.position + velo * Time.deltaTime);
+
     }
 
-    public void RotateCamera()
+    private void RotateCamera()
     {
-        float mouseX = Input.GetAxis("Mouse Y");
-        float mouseY = Input.GetAxis("Mouse X");
+        mouseX = Input.GetAxis("Mouse Y");
+        mouseY = Input.GetAxis("Mouse X");
 
         currentRotate -= mouseX * rotateSpeed;
 
@@ -47,5 +69,40 @@ public class PlayerController : MonoBehaviour
 
         transform.localRotation *= Quaternion.Euler(0, mouseY * rotateSpeed, 0);
         cam.transform.localEulerAngles = new Vector3(currentRotate, 0f, 0f);
+    }
+
+    private void Cling()
+    {
+        if (canCling)
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                rigidbody.drag = 100;
+                inputZ = 0;
+                canClimbing = true;
+            }
+            else if(Input.GetKeyUp(KeyCode.LeftShift))
+            { 
+                rigidbody.drag = 0;
+                canClimbing = false;
+            }
+        }
+    }
+
+    private void Jump()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        canCling = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        canCling = false;
+        canClimbing = false;
+        rigidbody.drag = 0;
     }
 }
